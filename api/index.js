3ba4474d23032391ea681ea46a215484257f964f
Sithija Shehara <nsithijashehara@gmail.com>
2024-05-23 12:47:51 +0530

    lesson 2 - middleware

--- api/index.js
+++ api/index.js
@@ -1,18 +1,33 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import { router } from "./routes/index.js";
+import { logger } from "./middleware/logger.js";
+import { errorHandler } from "./middleware/errorHandler.js";
+import { corsOptions } from "./config/corsOptions.js";
 
 const app = express();
-const PORT = process.env.PORT || 3500;
+const PORT = process.env.PORT || 5000;
 
 // Resolve __dirname in ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// logger
+app.use(logger);
+
+// cors
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+app.use(cookieParser());
+
 app.use("/", express.static(path.join(__dirname, "/public")));
 
-// Import the routes
-import { router } from "./routes/index.js";
 app.use("/", router);
 
 app.all("*", (req, res) => {
@@ -25,5 +40,7 @@ app.all("*", (req, res) => {
     res.type("txt").send("404 Not Found");
   }
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
