9ed2aa32b48161c053436c5346c4f7da93d3313f
Sithija Shehara <nsithijashehara@gmail.com>
2024-05-27 22:41:24 +0530

    minor code change

--- api/config/allowedOrigins.js
+++ api/config/allowedOrigins.js
@@ -1,5 +1,1 @@
-export const allowedOrigins = [
-  "http://localhost:3000",
-  "https://www.dandrepairshop.com",
-  "https://dandrepairshop.com",
-];
+export const allowedOrigins = ["http://localhost:3000"];
